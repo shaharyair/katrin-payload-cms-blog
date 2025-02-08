341073d2cf61d8da6d3964fc8d356d2a61d56325
Shahar Yair <shahar@firstofferz.com>
2025-02-08 17:46:20 +0200

    fix desktop navbar style

--- src/Header/Nav/index.tsx
+++ src/Header/Nav/index.tsx
@@ -12,10 +12,12 @@ export const HeaderNav: React.FC<{ data: HeaderType }> = ({ data }) => {
   const navItems = data?.navItems || [];
 
   return (
-    <nav className="flex items-center gap-3">
-      {navItems.map(({ link }, i) => {
-        return <CMSLink key={i} {...link} appearance="link" />;
-      })}
+    <nav className="mr-8 flex w-full items-center justify-between gap-3">
+      <div className="flex items-center justify-center gap-3">
+        {navItems.map(({ link }, i) => {
+          return <CMSLink key={i} {...link} appearance="link" />;
+        })}
+      </div>
       <Link href="/search">
         <span className="sr-only">Search</span>
         <SearchIcon className="w-5 text-primary" />
