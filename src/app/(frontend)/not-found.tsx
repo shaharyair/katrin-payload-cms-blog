146c014757bd1c54034b29d4a8f8cf87c02f4870
Shahar Yair <shahar@firstofferz.com>
2025-03-23 21:49:25 +0200

    update not found page

--- src/app/(frontend)/not-found.tsx
+++ src/app/(frontend)/not-found.tsx
@@ -7,10 +7,10 @@ export default function NotFound() {
     <div className="container py-28">
       <div className="prose max-w-none">
         <h1 style={{ marginBottom: 0 }}>404</h1>
-        <p className="mb-4">This page could not be found.</p>
+        <p className="mb-4">העמוד לא נמצא</p>
       </div>
       <Button asChild variant="default">
-        <Link href="/">Go home</Link>
+        <Link href="/">חזרה לעמוד הבית</Link>
       </Button>
     </div>
   );
