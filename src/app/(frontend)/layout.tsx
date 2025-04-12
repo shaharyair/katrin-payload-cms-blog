5c798776fbcdc600e41cd36c1cea90d33665abe5
Shahar Yair <shaharyair12@gmail.com>
2025-04-12 14:05:30 +0300

    add google analytics

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -9,6 +9,7 @@ import { Providers } from "@/providers";
 import { InitTheme } from "@/providers/Theme/InitTheme";
 import { getServerSideURL } from "@/utilities/getURL";
 import { mergeOpenGraph } from "@/utilities/mergeOpenGraph";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Open_Sans } from "next/font/google";
 import { Userway } from "../../components/common/userway";
@@ -51,6 +52,7 @@ export default async function RootLayout({
           <Footer />
         </Providers>
         <Userway />
+        <GoogleAnalytics gaId="G-YRP46P1X8Q" />
       </body>
     </html>
   );
