2082dbafa4b7a049df25e8f0eb86bde2adf11fa9
Shahar Yair <shaharyair12@gmail.com>
2025-04-12 12:51:18 +0300

    add katrin favicon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -32,7 +32,7 @@ export default async function RootLayout({
     >
       <head>
         <InitTheme />
-        <link href="/favicon.ico" rel="icon" sizes="32x32" />
+        {/* <link href="/favicon.ico" rel="icon" sizes="32x32" /> */}
         <link href="/favicon.svg" rel="icon" type="image/svg+xml" />
       </head>
       <body>
